abe247870ac2b8f3e7c8655f9a417f6cb81e1997
Emmett Naughton <enaughton5@gmail.com>
2018-08-22 21:14:18 -0400

    Final changes added.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-
+//Variables 
 var students = document.querySelectorAll('.student-item');
 var details = document.querySelectorAll('.student-details');
 var pageNum = 1;
 
-//function that takes a pageNumber and displays all the students, 10 per page
+//showPage takes a pageNumber and displays the students, 10 per page
 function showPage(pageNum, students) {
 	const upper = (pageNum * 10) - 1;
 	const lower = (pageNum * 10) - 10;
@@ -16,12 +16,8 @@ function showPage(pageNum, students) {
 	}
 }
 
-	// then loop through all students in list in our student list argument
-	// if student should be on this page number
-	// show the student
 	
-
-	//The section below adds page links based on how many students are in the list.
+//The section below adds page links based on how many students are in the list.
 	var div = document.createElement('div');
 	var ul = document.createElement('ul');
 	
@@ -31,56 +27,55 @@ function showPage(pageNum, students) {
 
 	
 	
-
+//createPageLinks takes the variable pageNum and dispays the correct number of pages 
 function createPageLinks(pageNum) {
 	
 	let li = document.createElement('li');
-
+	
 	li.innerHTML = '<a href="#">' + pageNum + '</a>';
 	ul.appendChild(li);
+
 	let links = ul.lastChild;
 
+	//the eventListener adds/removes the class of action to the correct anchor tag
 	links.addEventListener('click', () =>{
-			showPage(pageNum, students)	
-
-	
-			
-	})
-
 		
+		showPage(pageNum, students)	
+		const anchors = document.querySelectorAll("a");
+      
+    	for (let i = 0; i < anchors.length; i++) {
+        	anchors[i].classList.remove("active");
+        	event.target.classList.add("active");
+        }	
+	})
+}
+
+
+//appendPageLinks takes in studets as an argument, 
+function appendPageLinks(students){
+
+	let pages = Math.ceil(students.length/10);
 	
-	}
+	for(let i = 0 ; i < pages; i++){
 
-function appendPageLinks(students) {
-	let page = Math.ceil(students.length/10);
-
-	for(let i = 0 ; i < page; i++){
 		createPageLinks(pageNum)
 		pageNum += 1;
 	}
 
 
-
-
- 
 }
-
-
-
-// determine how many pages for this student list
-// create a page link section	
-// "for" every page
-// add a page link to the page link section
-// remove the old page link section from the site
-// append our new page link section to the site
-// define what happens when you click a link (event listener)
-// Use showPage to display the page for the link clicked
-// mark that link as "active
-
-
-//document.getElementsByTagName('li').addEventListener('click', showPage(pageNum));
 
 showPage(pageNum, students);
 appendPageLinks(students);
-//createPageLinks(pageNum);
 
+
+
+
+
+  
+
+
+
+
+
+	
